b57250ce2945f9f1a9dc4219a39a95f8d59c0199
R2m6nshuP <rra443564@gmail.com>
2026-08-29 21:48:57 +0530

    Add Adjacent Replacements [A] from Codeforces

---
+++ 1006/A - Adjacent Replacements/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int n;
+cin>>n;
+while(n--){
+int k;
+cin>>k;
+if(k%2==0) k--;
+cout<<k<<" ";
+}
+cout<<"
+";
+}
